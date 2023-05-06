7bf9299c56311a0651e62717ec8386b03f327b0f
jpa96 <100251959+jpa96@users.noreply.github.com>
2023-05-06 22:51:28 +1000

    Added Footer and Anchor link to Github Repository

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import Header from "./Header.js";
 import WeatherDisplay from "./WeatherDisplay.js";
+import Footer from "./Footer.js"
 
 function App() {
   return (
     <div className="App">
       <Header />
       <WeatherDisplay />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <div className="Footer">
+            <footer><a href="https://github.com/jpa96/react-weather-app" target="blank" alt="link to GitHub Repository">Open-source code</a>, by Jessica Anstey</footer>
+        </div>
+    )
+}
